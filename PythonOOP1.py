492dab7954ea18be6f719aadabd65cca4768c916
yashpanchal01 <yashpanchal251198@gmail.com>
2025-09-12 08:09:57 +0530

    Comment Notes Added

--- PythonOOP1.py
+++ PythonOOP1.py
@@ -1,1 +1,8 @@
-print("Hellow World!")
+# When to use OBJECTS and Classes ? 
+# Whenever There are Objects in Context, Such as Phone, book, Cup
+# Attributes(Variables) are Properties of Objects -> For a Phone => Model_Number = 12091314, Turned_On = True, Price = 50krs.
+# Methods(Functions) are Functions That belong to and Object, Methods = Functions  -> For a Phone, Def Make_call():
+#  Def Recive_call(): Def Turn_on                 
+#
+#                           For a cup => Is_empty, Temprature, Liquid
+#
